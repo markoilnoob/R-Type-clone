6f0a9de2686708b431007e6f6fc121c7ea86fa89
Lodo Marco <marco.lodo1010@gmail.com>
2025-02-23 21:19:16 +0100

    Created collision to enemies
    Updated enemy check for collision with projectile
    Updated Project settings for build

---
+++ Assets/Code/Scripts/Enemies/EnemyCollision.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class EnemyCollision : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("PlayerBullet"))
+        {
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
+    }
+}
--- Assets/Code/Scripts/Player/Bullet.cs
+++ Assets/Code/Scripts/Player/Bullet.cs
@@ -38,5 +38,5 @@ public class Bullet : MonoBehaviour
         }
     }
 
-    private void OnCollisionEnter2D( Collision2D other ) => Destroy(gameObject);
+    //private void OnCollisionEnter2D( Collision2D other ) => Destroy(gameObject);
 }
